ce5e591d87d6202b3ec24221817c0cf0ead5c90e
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-01 23:34:31 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -2,25 +2,32 @@
 
 pid=$(pgrep 'rbbc')
 #localHeight=$(curl -s localhost:8080/metrics | grep -v "#" | grep rbn_database_chaindb_height | awk '{print $2}')
-localHeight=$(cat $HOME/rb/logs/rbbcLogs | grep "Imported new chain segment" | tail -1 | awk -F 'number":"' '{print $2}' | cut -d '"' -f 1)
-netHeight=$(echo $(( 16#$(curl -s https://rbn-gcp-australia-southeast1-a-0-b-v2.devnet.redbelly.network:8545/ -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}' | jq -r .result.number | sed 's/0x//') )) )
-type="-"
-ver="-"
+local_height=$(cat ~/rb/logs/rbbcLogs | grep "Imported new chain segment" | tail -1 | awk -F 'number":"' '{print $2}' | cut -d '"' -f 1)
+net_height=$(echo $(( 16#$(curl -s https://rbn-gcp-australia-southeast1-a-0-b-v2.devnet.redbelly.network:8545/ -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}' | jq -r .result.number | sed 's/0x//') )) )
 network="devnet2"
-isGovernor=$(cat ~/rb/logs/rbbcLogs | grep "IsGovernor" | tail -1 | awk -F 'IsGovernor: ' '{print $2}' | cut -d '"' -f 1)
+is_governor=$(cat ~/rb/logs/rbbcLogs | grep "IsGovernor" | tail -1 | awk -F 'IsGovernor: ' '{print $2}' | cut -d '"' -f 1)
 
-if (( $localHeight == $netHeight )); then status="ok";note="governor:$isGovernor"; else status="warning";note=" syncing $localHeight/$netHeight"; fi
+if (( $local_height == $net_height )); then status="ok";note="governor:$is_governor"; else status="warning";note=" syncing $local_height/$net_height"; fi
 if [ -z $pid ]; then status="error";note="not running"; fi
-foldersize=$(du -hs ~/rb | awk '{print $1}')
-logsize=$(du -hs ~/rb/logs/rbbcLogs | awk '{print $1}')
+folder_size=$(du -hs ~/rb | awk '{print $1}')
+log_size=$(du -hs ~/rb/logs/rbbcLogs | awk '{print $1}')
+log=$(cat ~/rb/logs/rbbcLogs | tail -5)
 
-echo "updated='$(date +'%y-%m-%d %H:%M')'"
-echo "version='$ver'"
-echo "process='$pid'"
-echo "status="$status
-echo "note='$note'"
-echo "network='$network'"
-echo "type="$type
-echo "folder="$foldersize
-echo "log="$logsize
-echo "governor=$isGovernor" 
+cat << EOF
+{
+  "project":"$folder",
+  "id":$ID,
+  "machine":"$MACHINE",
+  "chain":"devnet2",
+  "type":"node",
+  "status":"$status",
+  "note":"$note",
+  "updated":"$(date --utc +%FT%TZ)",
+  "folder_size":"$folder_size",
+  "log_size":"$log_size",
+  "logs": 
+  { "message":"$(echo $logs | tail -1 | head -1')",
+    "message":"$(echo $logs | tail -2 | head -1')",
+    "message":"$(echo $logs | tail -3 | head -1')" }
+}
+EOF

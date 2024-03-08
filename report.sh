d9acb9f8433e269d3475876695d7eae6fca19097
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-08 22:46:12 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ~/.bash_profile
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 pid=$(pgrep 'rbbc')
@@ -12,16 +13,18 @@ if [ -z $pid ]; then status="error";note="not running"; fi
 folder_size=$(du -hs ~/rb | awk '{print $1}')
 log_size=$(du -hs ~/rb/logs/rbbcLogs | awk '{print $1}')
 log1=$(cat ~/rb/logs/rbbcLogs | tail -1 | sed 's/\"/\\\"/g' )
+id=redbelly-$REDBELLY_ID
+chain=devnet2
 
 cat << EOF
 {
   "project":"$folder",
-  "id":"$ID",
+  "id":"$id",
   "machine":"$MACHINE",
-  "chain":"devnet2",
+  "chain":"$chain",
   "type":"node",
   "status":"$status",
-  "note":"$note",
+  "message":"$message",
   "is_governor":"$is_governor",
   "updated":"$(date --utc +%FT%TZ)",
   "folder_size":"$folder_size",
@@ -31,3 +34,16 @@ cat << EOF
   ]
 }
 EOF
+
+# send data to influxdb
+if [ ! -z $INFLUX_HOST ]
+then
+ curl --request POST \
+ "$INFLUX_HOST/api/v2/write?org=$INFLUX_ORG&bucket=$REDBELLY_BUCKET&precision=ns" \
+  --header "Authorization: Token $INFLUX_TOKEN" \
+  --header "Content-Type: text/plain; charset=utf-8" \
+  --header "Accept: application/json" \
+  --data-binary '
+    status,node='$id',machine='$MACHINE' status="'$status'",message="'$message'",version="'$version'",url="'$url'",chain="'$chain'" '$(date +%s%N)' 
+    '
+fi

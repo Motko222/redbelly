434f1693aa1f9a8b82191a7a4f27d5b6d7aef225
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-03-09 00:07:52 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -3,9 +3,10 @@ source ~/.bash_profile
 
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 pid=$(pgrep 'rbbc')
+rpc=https://rbn-gcp-australia-southeast1-a-0-b-v2.devnet.redbelly.network:8545
 #localHeight=$(curl -s localhost:8080/metrics | grep -v "#" | grep rbn_database_chaindb_height | awk '{print $2}')
 local_height=$(cat ~/rb/logs/rbbcLogs | grep "Imported new chain segment" | tail -1 | awk -F 'number":"' '{print $2}' | cut -d '"' -f 1)
-#net_height=$(echo $(( 16#$(curl -s https://rbn-azure-australiasoutheast-1-0-b.devnet.redbelly.network:8545/ -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}' | jq -r .result.number | sed 's/0x//') )))
+net_height=$(echo $(( 16#$(curl -s $rpc -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}' | jq -r .result.number | sed 's/0x//') )))
 is_governor=$(cat ~/rb/logs/rbbcLogs | grep "IsGovernor" | tail -1 | awk -F 'IsGovernor: ' '{print $2}' | cut -d '"' -f 1)
 
 if (( $local_height == $net_height )); then status="ok";message="governor:$is_governor"; else status="warning";message=" syncing $local_height/$net_height"; fi

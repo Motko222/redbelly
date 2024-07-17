e49cfda2600513fe59e9336799b7c67c32ebdd0d
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-17 10:01:35 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -2,10 +2,18 @@
 source ~/.bash_profile
 
 pid=$(pgrep 'rbbc')
-rpc=https://rbn-gcp-australia-southeast1-a-0-b-v2.devnet.redbelly.network:8545
-#localHeight=$(curl -s localhost:8080/metrics | grep -v "#" | grep rbn_database_chaindb_height | awk '{print $2}')
-local_height=$(cat ~/rb/logs/rbbcLogs | grep "Imported new chain segment" | tail -1 | awk -F 'number":"' '{print $2}' | cut -d '"' -f 1)
-net_height=$(echo $(( 16#$(curl -s $rpc -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}' | jq -r .result.number | sed 's/0x//') )))
+local_rpc=$REDBELLY_RPC
+net_rpc=https://rbn-gcp-australia-southeast1-a-0-b-v2.devnet.redbelly.network:8545
+owner=$REDBELLY_OWNER
+id=$REDBELLY_ID
+network=devnet
+chain=devnet2
+group=node
+
+# localHeight=$(curl -s localhost:8080/metrics | grep -v "#" | grep rbn_database_chaindb_height | awk '{print $2}')
+#local_height=$(cat ~/rb/logs/rbbcLogs | grep "Imported new chain segment" | tail -1 | awk -F 'number":"' '{print $2}' | cut -d '"' -f 1)
+local_height=$(echo $(( 16#$(curl -s $local_rpc -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}' | jq -r .result.number | sed 's/0x//') )))
+net_height=$(echo $(( 16#$(curl -s $net_rpc -X POST -H "Content-Type: application/json" --data '{"method":"eth_getBlockByNumber","params":["latest",false],"id":1,"jsonrpc":"2.0"}' | jq -r .result.number | sed 's/0x//') )))
 is_governor=$(cat ~/rb/logs/rbbcLogs | grep "IsGovernor" | tail -1 | awk -F 'IsGovernor: ' '{print $2}' | cut -d '"' -f 1)
 
 if (( $local_height == $net_height )); then status="ok";message="governor:$is_governor"; else status="warning";message=" syncing $local_height/$net_height"; fi
@@ -13,10 +21,6 @@ if [ -z $pid ]; then status="error";note="not running"; fi
 folder_size=$(du -hs ~/rb | awk '{print $1}')
 log_size=$(du -hs ~/rb/logs/rbbcLogs | awk '{print $1}')
 log1=$(cat ~/rb/logs/rbbcLogs | tail -1 | sed 's/\"/\\\"/g' )
-id=$REDBELLY_ID
-network=devnet
-chain=devnet2
-group=node
 
 cat << EOF
 {
@@ -47,6 +51,6 @@ then
   --header "Content-Type: text/plain; charset=utf-8" \
   --header "Accept: application/json" \
   --data-binary "
-    report,id=$id,machine=$MACHINE,grp=$group status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\",network=\"$network\" $(date +%s%N) 
+    report,id=$id,machine=$MACHINE,grp=$group,owner=$owner status=\"$status\",message=\"$message\",version=\"$version\",url=\"$url\",chain=\"$chain\",network=\"$network\" $(date +%s%N) 
     "
 fi

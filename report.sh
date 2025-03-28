b02324692cccfd6f78b60f1f5738629feb8b7cc8
Motko <77466238+Motko222@users.noreply.github.com>
2025-03-28 14:12:55 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -19,7 +19,11 @@ behind=$(( $net_height - $local_height))
 folder_size=$(du -hs /opt/redbelly | awk '{print $1}')
 log_size=$(du -hs /var/log/redbelly | awk '{print $1}')
 
-[ $behind -gt 5 ] && { status="ok" && message="governor:$is_governor" } || { status="warning" && message=" syncing $local_height/$net_height (behind $behind)" }
+if [ $behind -gt 5 ] then
+   status="ok" && message="governor:$is_governor"
+else 
+   status="warning" && message=" syncing $local_height/$net_height (behind $behind)"
+fi
 [ $errors -gt 100 ] && status="warning" && message="errors=$errors behind=$behind";
 [ -z $pid ] && status="error" && note="process not running"
 
